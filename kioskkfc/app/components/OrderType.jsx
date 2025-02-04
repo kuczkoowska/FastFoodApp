658735a24c7c5cd3658fa2e350eac7fdc78b5296
Jagoda Kuczkowska <jagoda.kucz@wp.pl>
2025-02-04 08:49:49 +0100

    bug fix

--- kioskkfc/app/components/OrderType.jsx
+++ kioskkfc/app/components/OrderType.jsx
@@ -9,7 +9,7 @@ import TakeAwayIcon from "../../public/takeaway.ico.svg";
 const listItemClass =
   "flex flex-col w-36 h-28 rounded-2xl shadow-md justify-center items-center uppercase text-black font-bold md:w-64 md:h-52";
 
-const here = "bg-zinc-100 hover:text-white hover:bg-red-600 hover:scale-105 hover:transition-transform hover:duration-300"
+const here = " bg-zinc-100 hover:text-white hover:bg-red-600 hover:scale-105 hover:transition-transform hover:duration-300"
 
 const orderTypes = [
   { href: "/menu", icon: DeliveryIcon, label: "Delivery" },
@@ -41,7 +41,7 @@ const OrderType = () => {
               <p>{orderType.label}</p>
             </li>
           ) : (
-            <Link key={index} href={orderType.href} className={listItemClass}>
+            <Link key={index} href={orderType.href} className={listItemClass + here}>
               <div className="hidden md:block">
                 <orderType.icon width="125" height="125" />
               </div>
